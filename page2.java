92cafbe0a62bc99e2e3e5fab5bae2295fd47e034
muhammed shamal <109429883+theshamalsaleem@users.noreply.github.com>
2022-07-16 22:10:14 +0530

    Create page2.java

---
+++ page2.java
@@ -0,0 +1,14 @@
+package com.ksebmeterreader.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class page_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+    }
+}
